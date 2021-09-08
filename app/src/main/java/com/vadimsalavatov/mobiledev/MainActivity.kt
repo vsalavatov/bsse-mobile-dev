a8b00a06f3153580afb15e44856c6e925f015df5
Vadim Salavatov <vmsalavatov@gmail.com>
2021-09-08 16:54:56 +0300

    +users recycler view (practice)

--- app/src/main/java/com/vadimsalavatov/mobiledev/MainActivity.kt
+++ app/src/main/java/com/vadimsalavatov/mobiledev/MainActivity.kt
@@ -2,10 +2,35 @@ package com.vadimsalavatov.mobiledev
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val usersRecyclerView = findViewById<RecyclerView>(R.id.usersRecyclerView)
+        usersRecyclerView.layoutManager = LinearLayoutManager(this, RecyclerView.VERTICAL, false)
+        val userAdapter = UserAdapter()
+        usersRecyclerView.adapter = userAdapter
+        userAdapter.userList = loadUsers()
+        userAdapter.notifyDataSetChanged()
+    }
+
+    fun loadUsers(): List<User> {
+        return listOf(
+            User("avatar url", "биба", "б09"),
+            User("avatar url2", "боба", "б10"),
+            User("avatar url", "биба", "б09"),
+            User("avatar url2", "боба", "б10"),
+            User("avatar url", "биба", "б09"),
+            User("avatar url2", "боба", "б10"),
+            User("avatar url", "биба", "б09"),
+            User("avatar url2", "боба", "б10"),
+            User("avatar url", "биба", "б09"),
+            User("avatar url2", "боба", "б10"),
+            User("avatar url", "биба", "б09"),
+            User("avatar url2", "боба", "б10"),
+        )
     }
 }
