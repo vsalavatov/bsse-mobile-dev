65e74592a1cc45bae0692d5036ec2582e20dfc84
Vadim Salavatov <vmsalavatov@gmail.com>
2021-12-06 18:54:49 +0300

    +logger & navigation backlog

--- app/src/main/java/com/vadimsalavatov/mobiledev/BaseFragment.kt
+++ app/src/main/java/com/vadimsalavatov/mobiledev/BaseFragment.kt
@@ -2,8 +2,24 @@ package com.vadimsalavatov.mobiledev
 
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import timber.log.Timber
 
 open class BaseFragment : Fragment {
     constructor(): super()
     constructor(@LayoutRes contentLayoutId: Int): super(contentLayoutId)
+
+    override fun onStart() {
+        super.onStart()
+        if (BuildConfig.DEBUG) {
+            val logTag = "NavigationInfo"
+            logFragmentHierarchy(logTag)
+            try {
+                findNavController().logBackstack(logTag)
+            } catch (error: IllegalStateException) {
+                Timber.e(error)
+            }
+        }
+    }
+
 }
