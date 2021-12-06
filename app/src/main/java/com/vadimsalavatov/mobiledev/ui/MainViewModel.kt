0ec3a1ace7915ecc977d1ec9627d512a3380f5fe
Vadim Salavatov <vmsalavatov@gmail.com>
2021-12-06 20:19:17 +0300

    navigation graphs

--- app/src/main/java/com/vadimsalavatov/mobiledev/ui/MainViewModel.kt
+++ app/src/main/java/com/vadimsalavatov/mobiledev/ui/MainViewModel.kt
@@ -1,5 +1,10 @@
 package com.vadimsalavatov.mobiledev.ui
 
 import com.vadimsalavatov.mobiledev.ui.base.BaseViewModel
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
 
-class MainViewModel : BaseViewModel()
+
+class MainViewModel : BaseViewModel() {
+    val isAuthorizedFlow: Flow<Boolean> = MutableStateFlow(true)
+}
