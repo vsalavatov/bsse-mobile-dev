1d0f6cb658289f78b388282052e3e94fd3b11eb1
Vadim Salavatov <vmsalavatov@gmail.com>
2021-12-09 21:08:34 +0300

    email confirmation code edit field

--- app/src/main/java/com/vadimsalavatov/mobiledev/ui/emailconfirmation/EmailConfirmationFragment.kt
+++ app/src/main/java/com/vadimsalavatov/mobiledev/ui/emailconfirmation/EmailConfirmationFragment.kt
@@ -1,6 +1,11 @@
 package com.vadimsalavatov.mobiledev.ui.emailconfirmation
 
+import android.app.AlertDialog
+import android.os.Bundle
+import android.view.View
+import androidx.activity.addCallback
 import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.vadimsalavatov.mobiledev.R
 import com.vadimsalavatov.mobiledev.databinding.FragmentEmailConfirmationBinding
@@ -11,4 +16,11 @@ class EmailConfirmationFragment : BaseFragment(R.layout.fragment_email_confirmat
     private val viewBinding by viewBinding(FragmentEmailConfirmationBinding::bind)
 
     private val viewModel: EmailConfirmationViewModel by viewModels()
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewBinding.backButton.setOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
 }
